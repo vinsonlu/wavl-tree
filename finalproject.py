f7b28d1db221bccf6cac965460369ea995fa095e
Intehl <vinsonlu2@gmail.com>
2022-05-03 05:17:43 -0500

    Started a bit

---
+++ finalproject.py
@@ -0,0 +1,16 @@
+class WAVLTree:
+    def __init__(self):
+        self.root = WAVLNode()
+
+
+class WAVLNode:
+    def __init__(self, key, value):
+        self.parent = parent
+        self.left_child = None
+        self.right_child = None
+        self.key = key
+        self.value = value
+
+
+    
+    
