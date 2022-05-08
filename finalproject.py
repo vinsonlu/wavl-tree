471f600a0ccbc26a72afb650518ba1c3e86ada3a
Intehl <vinsonlu2@gmail.com>
2022-05-08 18:39:29 -0500

    Fixed a few bugs, and example usage is demonstrated!

--- finalproject.py
+++ finalproject.py
@@ -4,6 +4,8 @@ import random
 
 from collections import deque
 
+### HELPER FOR VISUALIZATION
+
 def hierarchy_pos(G, root=None, width=1., vert_gap = 0.2, vert_loc = 0, xcenter = 0.5):
     '''
     From Joel's answer at https://stackoverflow.com/a/29597209/2966723.  
@@ -68,11 +70,28 @@ def hierarchy_pos(G, root=None, width=1., vert_gap = 0.2, vert_loc = 0, xcenter 
             
     return _hierarchy_pos(G, root, width, vert_gap, vert_loc, xcenter)
 
+### TREE AND NODE CLASSES
+
 class WAVLTree:
+    """
+    Representation of a full WAVLTree. The tree is represented by pointers to
+    WAVLNode objects. Contains a root attribute, which represents the node
+    where the WAVLTree is rooted.
+    """
+    
     def __init__(self):
+        """
+        Creates a new WAVL tree.
+
+        Inputs:
+            - self: the tree to be constructed
+        """
         self.root = None
 
     def __str__(self):
+        """
+        Simple string representation of a tree.
+        """
         if self.root is None:
             return str(None)
 
@@ -113,6 +132,9 @@ class WAVLTree:
         return str(representation)
 
     def display(self):
+        """
+        Displays the tree using matplotlib and networkx.
+        """
         graph = nx.Graph()
         
         def build_graph(node):
@@ -133,12 +155,31 @@ class WAVLTree:
             print("Graph has no root!")
 
     def contains(self, key):
+        """
+        Checks whether the tree contains the given key.
+
+        Inputs:
+            - self: the tree to search
+            - key: the key to search for
+
+        Returns: true if the key is in the tree, and false otherwise
+        """
         if not self.root:
             return False
 
-        return not self.root.search(key)
+        return self.root.search(key) != False
 
     def get(self, key):
+        """
+        Gets the value associated with a given key. Raises a KeyError
+        if the key is not present in the tree.
+
+        Inputs:
+            - self: the tree to search
+            - key: the key to search for
+
+        Returns: the value mapped to the key.
+        """
         if not self.root:
             raise KeyError
         
@@ -150,6 +191,18 @@ class WAVLTree:
         return value
 
     def insert(self, key, value = True):
+        """
+        Adds a value into the tree by the given key (and value, if provided).
+        The node is added in-order. Work is done to ensure that the WAVL rank
+        invariant is preserved.
+
+        Inputs:
+            - self: the tree to insert the value into
+            - key: the key to insert
+            - value: the value to map
+
+        Returns: nothing
+        """
         if self.root is None:
             self.root = WAVLNode(key, value)
         else:
@@ -157,13 +210,13 @@ class WAVLTree:
             x = self.root.insert(key, value)
 
             # Ensure rank-invariant here, doing rotations if needed
-            while (x.parent is not None and 
+            while (x.parent and 
                 (x.parent.child_rank_difference() == (0, 1) or
                 x.parent.child_rank_difference() == (1, 0))):
                 x.parent.promote()
                 x = x.parent
 
-            if (x.parent is not None and 
+            if (x.parent and 
                 (x.parent.child_rank_difference() == (0, 2) or
                 x.parent.child_rank_difference() == (2, 0))):
                 # Do rotations to preserve balancing
@@ -190,15 +243,28 @@ class WAVLTree:
                         z.demote()
 
     def pop(self, key):
+        """
+        Removes the provided key from the tree and returns the mapped value.
+        Raises a KeyError if the key is not present.
+
+        Inputs:
+            - self: the tree to pop from
+            - key: the key to remove
+
+        Returns: the value mapped to key
+        """
         if self.root is None:
             raise KeyError
 
         node = self.root.search(key)
 
         if not node:
+            print(key)
+            print(self)
+            self.display()
             raise KeyError
 
-        node = self.delete(node) # If successor is deleted, start there
+        node = self._delete(node) # If successor is deleted, start there
 
         x = node.parent
         value = node.value
@@ -218,9 +284,11 @@ class WAVLTree:
 
             # Step 1: demote
             while (x.rank_difference() == 3 and 
-                (y.rank_difference() == 2 or
-                y.child_rank_difference() == (2, 2))):
-                if y.rank_difference() == 2:
+                (x.child_rank_difference()[0] == 2 or
+                x.child_rank_difference()[1] == 2 or
+                (y and y.child_rank_difference() == (2, 2)))):
+                if (x.child_rank_difference()[0] == 2 or 
+                    x.child_rank_difference()[1] == 2):
                     x.parent.demote()
                 else:
                     y.demote()
@@ -232,16 +300,18 @@ class WAVLTree:
                 y = x.parent.right if x == x.parent.left else x.parent.left
 
             # Step 2: rotate (if necessary)
-            if x and x.rank_difference() == 3:
+            if x and y and x.rank_difference() == 3:
                 z = x.parent
                 v = y.left
                 w = y.right
                 if z.left == x:
-                    if w.rank_difference() == 1:
+                    if y.child_rank_difference()[1] == 1:
                         self.left_rotate(y)
                         y.promote()
                         z.demote()
-                    else:
+                        if z.is_leaf():
+                            z.demote()
+                    elif v:
                         self.double_rotate(v)
                         v.promote()
                         v.promote()
@@ -249,12 +319,13 @@ class WAVLTree:
                         z.demote()
                         z.demote()
                 else: # Mirrored from above
-                    if w.rank_difference() == 1:
+                    if y.child_rank_difference()[0] == 1:
                         self.right_rotate(y)
                         y.promote()
                         z.demote()
-                        # TODO: if z.is_leaf() then demote again?
-                    else:
+                        if z.is_leaf():
+                            z.demote()
+                    elif w:
                         self.double_rotate(w)
                         w.promote()
                         w.promote()
@@ -265,9 +336,28 @@ class WAVLTree:
         return value
 
     def remove(self, key):
+        """
+        Removes the provided key from the tree.
+        Raises a KeyError if the key is not present.
+
+        Inputs:
+            - self: the tree to remove from
+            - key: the key to remove
+
+        Returns: nothing
+        """
         self.pop(key)
         
-    def delete(self, node):
+    def _delete(self, node):
+        """
+        Deletes a specified node from the tree. Identical to BST removal.
+
+        Inputs:
+            - self: the tree that node is a part of
+            - node: the node to remove
+
+        Returns: the final removed node object
+        """
         if node.is_leaf():
             if node.parent is not None:
                 if node.parent.right == node:
@@ -306,18 +396,25 @@ class WAVLTree:
         else:
             # Find successor
             succ = node.successor()
-            if succ == node.right:
-                node.right = succ.right
             
-            # Swap node contents with successor
+            # Swap node contents with succe4ssor
             succ.key, node.key = node.key, succ.key
             succ.value, node.value = node.value, succ.value
             
             # Delete successor. Recursion has depth 1, since successor can't 
             # have two children
-            return self.delete(succ)
+            return self._delete(succ)
 
     def left_rotate(self, x):
+        """
+        Rotates the tree left, making the parent of x the left child of x.
+
+        Inputs:
+            - self: the tree to rotate
+            - x: the node to rotate higher
+
+        Returns: nothing
+        """
         t3 = x.left
         p = x.parent
 
@@ -340,6 +437,15 @@ class WAVLTree:
         p.parent = x
 
     def right_rotate(self, x):
+        """
+        Rotates the tree right, making the parent of x the right child of x.
+
+        Inputs:
+            - self: the tree to rotate
+            - x: the node to rotate higher
+
+        Returns: nothing
+        """
         t2 = x.right
         p = x.parent
 
@@ -362,6 +468,17 @@ class WAVLTree:
         p.parent = x
 
     def double_rotate(self, x):
+        """
+        Performs a double rotation on x, rotating it upwards and downwards
+        in that order.
+
+        Inputs:
+            - self: the tree to perform the rotation on
+            - x: the node to rotate on
+
+        Returns: nothing
+        """
+        
         if x == x.parent.left:
             self.right_rotate(x)
             self.left_rotate(x)
@@ -370,7 +487,20 @@ class WAVLTree:
             self.right_rotate(x)
 
 class WAVLNode:
+    """
+    Encapsulates the information within each node.
+    """
     def __init__(self, key, value = True):
+        """
+        Creates a new node object.
+
+        Inputs:
+            - self: the new node object being created
+            - key: the key of the node
+            - value: the value to map to the provided key
+
+        Returns: nothing
+        """
         self.left = None
         self.right = None
         self.parent = None
@@ -379,15 +509,34 @@ class WAVLNode:
         self.rank = 0
 
     def search(self, key):
+        """
+        Recursive search to find a key.
+
+        Inputs:
+            - self: the WAVLNode being checked
+            - key: the key to match
+
+        Returns: the node that holds the given key.
+        """
         if self.key == key:
             return self
 
         if self.key < key:
-            return self.right.search(key) if self.right != None else False
+            return self.right.search(key) if self.right is not None else False
         
-        return self.left.search(key) if self.right != None else False
+        return self.left.search(key) if self.left is not None else False
 
     def insert(self, key, value = True):
+        """
+        Performs a recursive insertion identical to BST insertion.
+
+        Inputs:
+            - self: the tree to insert into
+            - key: the key to insert
+            - value: the value to map to the provided key
+        
+        Returns: the new WAVLNode created (or updated)
+        """
         if self.key == key:
             self.value = value
             return self
@@ -409,26 +558,76 @@ class WAVLNode:
             return self.left
     
     def promote(self):
+        """
+        Increments the rank of a node by one.
+        
+        Inputs:
+            - self: the node to increment
+
+        Returns: nothing
+        """
         self.rank += 1
 
     def demote(self):
+        """
+        Decrements the rank of a node by one.
+        
+        Inputs:
+            - self: the node to decrement
+
+        Returns: nothing
+        """
         self.rank -= 1
 
     def is_leaf(self):
+        """
+        Checks whether the node is a leaf node.
+        
+        Inputs:
+            - self: the node to check
+
+        Returns: true if the node is a leaf, and false otherwise
+        """
         return self.left is None and self.right is None
 
     def child_rank_difference(self):
+        """
+        Finds the rank difference of its children.
+        
+        Inputs:
+            - self: the node whose children are being checked
+
+        Returns: the left child rank difference and the right child 
+        rank difference
+        """
         left = self.left.rank if self.left else -1
         right = self.right.rank if self.right else -1
 
         return self.rank - left, self.rank - right
 
     def rank_difference(self):
+        """
+        Finds the rank difference of the node, which is defined as the rank
+        of its parent minus its own rank.
+
+        Inputs:
+            - self: the node to find the rank difference of
+
+        Returns: the rank difference of node
+        """
         if not self.parent:
             return None
         return self.parent.rank - self.rank
 
     def successor(self):
+        """
+        Finds the successor of self, if it exists.
+
+        Inputs:
+            - self: the node to find the successor of
+
+        Returns: a WAVLNode if there exists a successor of self
+        """
         if self.right is None:
             return None
         succ = self.right
@@ -436,18 +635,22 @@ class WAVLNode:
             succ = succ.left
         return succ
 
-# Testing
-tree = WAVLTree()
-insert = [3, 1, 5, 0, 2, 4, 6]
-for i in insert:
-    tree.insert(i)
-tree.display()
+### TESTING
 
-tree = WAVLTree()
-for i in range(8):
-    tree.insert(i)
-tree.display()
+def show_random_WAVLTree(size, deletions = 0):
+    tree = WAVLTree()
+    insert = [i for i in range(size + deletions)]
+    random.shuffle(insert)
+    for num in insert:
+        tree.insert(num)
 
-for i in range(7, -1, -1):
-    tree.remove(i)
+    deleted = []
+    for num in random.sample(insert, deletions):
+        tree.remove(num)
+        deleted.append(num)
+
     tree.display()
+
+show_random_WAVLTree(50)
+show_random_WAVLTree(50, 150)
+show_random_WAVLTree(200, 800)
